3503808be1a5c11c45d06bcddbfb764f9603e302
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-04-27 20:30:21 +0530

    Update #1 - Multiples of 3 and 5.py

--- #1 - Multiples of 3 and 5.py
+++ #1 - Multiples of 3 and 5.py
@@ -7,3 +7,16 @@ for a0 in range(t):
         if i%3==0 or i%5==0:
             sum+=i
     print(sum)
+    
+#to reduce time -
+
+t=int(input())
+def ar(x):
+    return x*(x+1);
+for i in range(t):
+    n =int(input())
+    n -=1;
+    a=int(n/3);
+    b=int(n/5);
+    c=int(n/15);
+    print(int(int(3*ar(a) + 5*ar(b) - 15*ar(c))>>1));
