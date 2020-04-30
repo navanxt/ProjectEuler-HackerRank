a3ce766250a546a25db13ec600e7fd52934c76df
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-04-30 08:50:23 +0530

    Update #6 - Sum square difference.py

--- #6 - Sum square difference.py
+++ #6 - Sum square difference.py
@@ -11,3 +11,12 @@ for a0 in range(t):
         sum2+=(i*i)
     sumsq = (sum1*sum1)
     print(abs(sumsq-sum2))
+    
+#to reduce time-->
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    sum1=n*(n+1)*(2*n+1)//6
+    sum2=n*(n+1)//2
+    print(abs((sum2*sum2)-sum1))
